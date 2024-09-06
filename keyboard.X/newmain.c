f8e589c3369e312bb5551f7c84303110a5c0753b
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2024-09-06 10:13:29 -0500

    First Commit
    Nothing to see here

---
+++ keyboard.X/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: 18502
+ *
+ * Created on September 5, 2024, 5:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
